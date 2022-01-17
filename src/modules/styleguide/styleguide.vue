52ad37d0bda98b03fcb64ce603f85b2dfd9c89b9
Solislab.SonNguyen <son.nguyen@solislab.com>
2022-01-17 16:01:19 +0700

    SBT-1218 Update typography on Style guide

--- src/modules/styleguide/styleguide.vue
+++ src/modules/styleguide/styleguide.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="style-guide">
     <styleguide-colors></styleguide-colors>
+    <styleguide-typography></styleguide-typography>
   </div>
 </template>
 
 <script>
 import './styleguide.css'
 import StyleguideColors from './styleguide-colors.vue'
+import StyleguideTypography from './styleguide-typography.vue'
 
 export default {
   name: "Styleguide",
   components: {
-    StyleguideColors
+    StyleguideColors,
+    StyleguideTypography
   }
 }
 </script>
