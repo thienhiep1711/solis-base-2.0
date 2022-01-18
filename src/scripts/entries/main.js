9b38f0f481d52df8347f3a81a0a30fa5c53607ca
Hiep Nguyen <hiep.nguyen@solislab.com>
2022-01-18 10:37:18 +0700

    SBT-1225 Scaffold module hero

--- src/scripts/entries/main.js
+++ src/scripts/entries/main.js
@@ -1,6 +1,12 @@
 import {set, isIEorEdge, isTouch} from 'lib/utils'
+import { initComponent } from 'lib/components'
 
 import 'styles/main.css'
+
+import Hero from 'modules/hero/hero'
+import Picture from 'modules/picture/picture'
+initComponent(Hero, 'hero')
+initComponent(Picture, 'picture')
 
 document.addEventListener('DOMContentLoaded', () => {
   if (isIEorEdge()) set(document.body, 'ie')
