bc8f438be69bf3cdacedea9c1d7704ce06879a9f
Hiep Nguyen <hiep.nguyen@solislab.com>
2022-01-20 10:10:10 +0700

    Restructure styleguide page

--- src/modules/styleguide/styleguide.vue
+++ src/modules/styleguide/styleguide.vue
@@ -1,23 +1,11 @@
 <template>
-  <div class="style-guide">
-    <styleguide-colors></styleguide-colors>
-    <styleguide-typography></styleguide-typography>
-    <styleguide-button></styleguide-button>
-  </div>
+  <slot />
 </template>
 
 <script>
 import './styleguide.css'
-import StyleguideColors from './styleguide-colors.vue'
-import StyleguideTypography from './styleguide-typography.vue'
-import StyleguideButton from './styleguide-button.vue'
 
 export default {
-  name: "Styleguide",
-  components: {
-    StyleguideColors,
-    StyleguideTypography,
-    StyleguideButton
-  }
+  name: "Styleguide"
 }
 </script>
