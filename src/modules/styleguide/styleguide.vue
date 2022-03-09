775b604a72bb6c94ef341c119fb6faa7ae7f65d7
SonNguyen <son.nguyen@solislab.com>
2022-03-09 10:39:32 +0700

    MNS-8 Remove component not used on Style guide page

--- src/modules/styleguide/styleguide.vue
+++ src/modules/styleguide/styleguide.vue
@@ -1,18 +1,15 @@
 <template>
   <slot />
   <styleguide-form />
-  <styleguide-component />
 </template>
 <script>
 import './styleguide.css'
 import StyleguideForm from '../styleguide-form/styleguide-form.vue'
-import StyleguideComponent from '../styleguide-component/styleguide-component.vue'
 
 export default {
   name: 'StyleGuide',
   components: {
-    StyleguideForm,
-    StyleguideComponent
+    StyleguideForm
   }
 }
 </script>
